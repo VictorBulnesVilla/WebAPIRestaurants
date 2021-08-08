92564d71ca0cc76f67041d5113f3d0266406b18d
Bulnes V <vicbulnes88@gmail.com>
2021-08-07 19:08:03 -0700

    Sube proyecto

---
+++ Data/WebAPIRestaurantsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIRestaurants.Models;
+
+namespace WebAPIRestaurants.Data
+{
+    public class WebAPIRestaurantsContext : DbContext
+    {
+        public WebAPIRestaurantsContext (DbContextOptions<WebAPIRestaurantsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPIRestaurants.Models.Restaurant> Restaurant { get; set; }
+    }
+}
---
+++ Models/Restaurant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIRestaurants.Models
+{
+    public class Restaurant
+    {
+        public int restaurantID { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string locacionLatitude { get; set; }
+        public string locationLongitude { get; set; }
+        public string phone { get; set; }
+        public string pathImage { get; set; }
+        public string email { get; set; }
+        public int isActivate { get; set; }
+        public DateTime creationDate { get; set; }
+        public int mainBranchID { get; set; }
+        public int restaurantCategoryID { get; set; }
+        public int userID { get; set; }
+
+    }
+}
+
+
